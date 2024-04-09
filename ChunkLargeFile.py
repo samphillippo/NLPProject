71ebc8ee99cc6727143b52956f8b91fb549d9e70
Nick Selvitelli <nickselvitelli@gmail.com>
2024-04-09 15:39:45 -0400

    Updated chunk script to work on multiple directories

--- ChunkLargeFile.py
+++ ChunkLargeFile.py
@@ -1,4 +1,7 @@
 import os
+from time import time
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
 
 def utf8len(s):
     return len(s.encode('utf-8'))
@@ -28,12 +31,12 @@ def chunk_json_file_write_while_going(filepath, out_dir_prefix, json_MB_chunk=10
                 if compress:
                     os.system('xz {}'.format(out_path))
 
+                current_KB_size = 0
+                num_chunks_made += 1
+
                 out_path = '{}_{:03d}.json'.format(out_dir_prefix, num_chunks_made)
                 chunked_filepaths.append(out_path)
                 out_chunk_file = open(out_path, 'w')
-
-                current_KB_size = 0
-                num_chunks_made += 1
 
         out_chunk_file.close()
         if compress:
@@ -47,20 +50,40 @@ def chunk_xz_file(filepath):
     file_prefix = filepath.split('.json')[0]
     json_path = file_prefix + '.json'
     chunked_filepaths = chunk_json_file_write_while_going(json_path, file_prefix)
+    os.remove(json_path)
     return chunked_filepaths
 
 
 
-# def find_xz_files_above_size(folderpath, xz_MB_min_size=150):
-#     filenames = list(filter(lambda x: x.endswith('.xz') and ((os.path.getsize(folderpath + '/' + x) / 1000.0) > (xz_MB_min_size * 1000.0)), os.listdir(folderpath)))
-#     return filenames
+
+def find_xz_files_above_size(folderpath, xz_MB_min_size=150):
+    filenames = list(filter(lambda x: x.endswith('.xz') and ((os.path.getsize(folderpath + '/' + x) / 1000.0) > (xz_MB_min_size * 1000.0)), os.listdir(folderpath)))
+    return filenames
 
 
-# FOLDER = './test_json'
+
+def chunk_all_files_in_folder(directory):
+    files = find_xz_files_above_size(directory, xz_MB_min_size=100)
+
+    for file in files:
+        chunk_xz_file(directory + '/' + file)
+    return directory
+
+
+
+FOLDERS = ['./test_json/split_0', './test_json/split_1']
+
+
+if __name__ == '__main__':
+    with ProcessPoolExecutor(max_workers=2) as executor:
+        futures = [executor.submit(chunk_all_files_in_folder, folder) for folder in FOLDERS]
+        for future in as_completed(futures):
+            result = future.result()
+            print("Completed chunking {}".format(result))
 
 
 # if __name__ == '__main__':
-#     files = find_xz_files_above_size(FOLDER, xz_MB_min_size=150)
+#     files = find_xz_files_above_size(FOLDER, xz_MB_min_size=100)
 
 #     print("Found {} files to chunk".format(len(files)))
 
@@ -70,16 +93,8 @@ def chunk_xz_file(filepath):
 #         print("Chunking {} ({}/{})\n - Extracting file to json...".format(file, index + 1, len(files)))
 #         local_time = time()
 
-#         os.system('unxz {}'.format(FOLDER + '/' +file))
-
-#         print(" - Extracted xz file ({} secs)".format(round((time() - local_time), 2)))
-
-#         file_prefix = file.split('.')[0]
-#         json_path = FOLDER + '/' + file_prefix + '.json'
-
-#         num_chunks = chunk_json_file_write_while_going(json_path, FOLDER, file_prefix, json_MB_chunk=200)
-
-#         os.remove(json_path)
+#         chunked_files = chunk_xz_file(FOLDER + '/' + file)
+#         num_chunks = len(chunked_files)
 
 #         print(" - Split {} into {} chunks ({} secs)\n".format(file, num_chunks, round((time() - local_time), 2)))
 
